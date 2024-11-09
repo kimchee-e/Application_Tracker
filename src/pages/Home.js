c81342c4d5f92dbb317be0c9f9333372845d6a84
Luke Flannigan <nym15@txstate.edu>
2024-11-09 12:12:04 -0600

    Added basic landing page structure and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
+import '../styles/Home.css';
+
 const Home = () => {
-    return ( 
-        <h1>This will be the Home Page</h1>
+    return (
+        <div className="home-container">
+            <div className="opening-text">
+                <h1>Internship Tracker</h1>
+                <p>Simplify your internship search with our easy-to-use application tracker</p>
+                <button className="start-button" onClick={() => window.location.href = '/login'}>
+                    Get Started
+                </button>
+            </div>
+        </div>
     );
 }
- 
+
 export default Home;
