47bd23f45e414b6be00d117fa1413b7c1b6d1dee
Luke Flannigan <nym15@txstate.edu>
2024-12-10 06:17:20 -0600

    Added visa sponsorship json.

--- process_h1b_data.py
+++ process_h1b_data.py
@@ -3,12 +3,11 @@ import json
 
 df = pd.read_excel('h1b_data.xlsx')
 
+certified_cases = df[df['CASE_STATUS'].isin(['Certified', 'Certified-Withdrawn'])]
+
 visaSponsors = {}
-for employer, group in df.groupby('EMPLOYER_NAME'):
-    visaSponsors[employer.lower()] = {
-        'applications': len(group),
-        'name': employer
-    }
+for employer, group in certified_cases.groupby('EMPLOYER_NAME'):
+    visaSponsors[employer.lower()] = len(group)
 
 with open('src/utils/visaSponsors.json', 'w') as f:
     json.dump(visaSponsors, f, indent=2) 
